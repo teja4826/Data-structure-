0e787b1ac6bd2a128ffe96798c1f69cb056b68be
teja4826 <122436792+teja4826@users.noreply.github.com>
2023-09-01 08:25:41 +0530

    Create fibonacci series.cpp

---
+++ fibonacci series.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a=0,b=1,next,i=2,len;
+	printf("Enter the number :");
+	scanf("%d",&len);
+	printf("Fibonacci series:%d, %d",a,b);
+	while(i<len)
+	{
+		next=a+b;
+		printf(", %d",next);
+		a=b;
+		b=next;
+		i++;
+		
+	}
+	
+}
